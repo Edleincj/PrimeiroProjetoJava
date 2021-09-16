e6161d2384413b5e69d37e391edbf93df2002b0d
Edleincj <edleincj@gmail.com>
2021-09-16 10:41:24 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProjetoJava.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+public class PrimeiroProjetoJava {
+    public static void main(String[] args) {
+        System.out.println("Usando método FOR!");
+
+        for(int i = 0; i <= 10; i++){
+            System.out.println("I = "+i);
+        }
+        for(int x = 0; x <= 5; x++)
+            System.out.println("X = " + x);
+    }
+}
